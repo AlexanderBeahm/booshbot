ab792a3a2ee302f32ab1416e08901aaaa4993e90
Alex Beahm <alexanderbeahm@gmail.com>
2021-04-23 16:11:49 -0500

    Adding new functionality to connect to twitch API, send requests to IGDB

--- booshbot.py
+++ booshbot.py
@@ -1,7 +1,11 @@
 import discord
 import logging
 import os
-import feedparser
+import requests
+import time
+
+from igdb.wrapper import IGDBWrapper
+from igdb.igdbapi_pb2 import GameResult
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -11,18 +15,9 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 client = discord.Client()
-
-rym_rss = "https://rateyourmusic.com/~The_Booshnaw/data/rss"
-
-def parserss(feed):
-    formattedstring = '**Recent Activity:'
-    for item in feed["items"]:
-        itemstring = f'\n{item["title"]}\t{item["link"]}'
-        if len(formattedstring + itemstring) > 2000:
-            break
-        formattedstring = formattedstring + itemstring
-
-    return formattedstring
+igdb_wrapper = None
+next_twitch_refresh = 0.0
+twitch_token = ''
 
 @client.event
 async def on_ready():
@@ -37,13 +32,42 @@ async def on_message(message):
     if message.content.startswith('$shutdown'):
         await message.channel.send('Shutting down...')
         await client.close()
-    if message.content.startswith('$recent'):
-        feed = feedparser.parse(rym_rss)
-        feed_message = parserss(feed)
-        await message.channel.send(feed_message)
+    if message.content.startswith('$gamers'):
+        '''With a wrapper instance already created'''
+        refresh_token(os.environ['TWITCH_CLIENT_ID'], os.environ['TWITCH_CLIENT_SECRET'])
+        # JSON API request
+        byte_array = igdb_wrapper.api_request(
+            'games',
+            'fields id, name; offset 0; where platforms=48;'
+          )
+        # parse into JSON however you like...
+
+        # Protobuf API request
+        byte_array = igdb_wrapper.api_request(
+            'games.pb', # Note the '.pb' suffix at the endpoint
+            'fields id, name; offset 0; where platforms=48;'
+          )
+        games_message = GameResult()
+        games_message.ParseFromString(byte_array) # Fills the protobuf message object with the response
+        await message.channel.send(games_message)
+
+    
+def refresh_token(client_id, client_secret):
+    global next_twitch_refresh
+    global igdb_wrapper
+    if(next_twitch_refresh - 10000 <= time.process_time()):
+        refresh_url = "https://id.twitch.tv/oauth2/token"
+        response = requests.post(url= refresh_url, params={'client_id':client_id, 'client_secret':client_secret, 'grant_type':'client_credentials'})
+        refreshToken = response.json()
+        access_token = refreshToken['access_token']
+        expires_in = refreshToken['expires_in']
+        next_twitch_refresh = time.process_time() + expires_in
+        twitch_token = access_token
+        igdb_wrapper = IGDBWrapper(client_id, twitch_token)
 
 def main():
     logger.info(f"Token is: {os.environ['DISCORD_BOT_TOKEN']}")
+    refresh_token(os.environ['TWITCH_CLIENT_ID'], os.environ['TWITCH_CLIENT_SECRET'])
     client.run(os.environ['DISCORD_BOT_TOKEN'])
 
 logger.info("Starting up BooshBot...")
